b997719e5a344dc0d18167087c9f009a228b2d3e
QuangNH <quangnh@vnpt-technology.vn>
2022-07-22 10:53:58 +0700

    update fonts, images

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Define color
+class AppColors {
+  static const Color primaryColor = Color(0xffABC4FF);
+  static const Color secondaryColor = Color(0xffEDF2FB);
+  static const Color blackGray = Color(0xff777777);
+  static const Color textBlack = Color(0xff211500);
+  static const Color lightBlue = Color(0xffD7E3FC);
+  static const Color textGrey = Color(0xff898788);
+  static const Color lightGrey = Color(0xff828680);
+}
---
+++ lib/values/app_styles.dart
@@ -0,0 +1,3 @@
+/**
+ * Define style of Text, Butotn,...
+ */
