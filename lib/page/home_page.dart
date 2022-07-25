5c2c8303e9df98c928a65d7f413f6124bfb7f8e3
QuangNH <quangnh@vnpt-technology.vn>
2022-07-25 17:45:12 +0700

    create card item

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -11,8 +11,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: AppColors.secondaryColor,
       appBar: AppBar(
@@ -35,14 +40,110 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         width: double.infinity,
+        margin: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
-            Text(
-              'It is amazing how complete is the delision that beauty is goodness',
-              style: AppStyles.h5.copyWith(
-                color: AppColors.blackGray
+            Container(
+              height: size.height * 1/10,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'It is amazing how complete is the delision that beauty is goodness',
+                style: AppStyles.h5.copyWith(
+                  fontSize: 12,
+                  color: AppColors.blackGray
+                ),
               ),
-            )
+            ),
+            Container(
+              height: size.height * 2/3,
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.all(Radius.circular(24))
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          alignment: Alignment.centerRight,
+                          child: Image.asset(AppAssets.heart)
+                        ),
+                        RichText(
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.start,
+                          text: TextSpan(
+                            style: TextStyle(
+                              fontFamily: FontFamily.sen,
+                              fontSize: 89,
+                              fontWeight: FontWeight.bold,
+                              shadows: [
+                                BoxShadow(
+                                  color: Colors.black38,
+                                  offset: Offset(3, 6),
+                                  blurRadius: 6
+                                )
+                              ]
+                            ),
+                            text: 'B',
+                            children: [
+                              TextSpan(
+                                  style: TextStyle(
+                                      fontFamily: FontFamily.sen,
+                                      fontSize: 52,
+                                      fontWeight: FontWeight.bold,
+                                      shadows: [
+                                        BoxShadow(
+                                            color: Colors.black38,
+                                            offset: Offset(3, 6),
+                                            blurRadius: 6
+                                        )
+                                      ]
+                                  ),
+                                text: 'eautiful'
+                              )
+                            ]
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 24),
+                          child: Text(
+                            '"Think of all the beauty still left around you and be happy"',
+                            style: AppStyles.h4.copyWith(
+                              letterSpacing: 1,
+                              color: Colors.black
+                            )
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              height: 10,
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return buildIndicator(index == _currentIndex, size);
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -52,6 +153,24 @@ class _HomePageState extends State<HomePage> {
 
         },
         child: Image.asset(AppAssets.exchange),
+      ),
+    );
+  }
+
+  Widget buildIndicator(bool isActive, Size size) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: isActive ? size.width * 1/5 : 24,
+      decoration: BoxDecoration(
+        color: isActive ? AppColors.lightBlue : AppColors.lightGrey,
+        borderRadius: BorderRadius.all(const Radius.circular(12)),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black38,
+              offset: Offset(2, 3),
+              blurRadius: 3
+          )
+        ]
       ),
     );
   }
